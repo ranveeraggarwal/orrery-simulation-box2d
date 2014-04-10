131f32cc98978671af59785e902883c61f5ffd1c
devdeepray <devdeep.ray1994@gmail.com>
2014-04-10 13:16:18 +0530

    Added generation scripts

---
+++ cs296_base_code/scripts/g07_gen_html.py
@@ -0,0 +1,70 @@
+import os, re, subprocess
+texfile=open("./doc/tex/cs296_report_07.tex",'r');
+htmfile=open("./doc/g07_prof_htm.html",'w');
+lw=texfile.readlines();
+lw=[x.replace("\\\\" , "<br>") for x in lw]
+lw=[x.strip() for x in lw]
+
+print("<!DOCTYPE html>", file=htmfile)
+print("<html>", file=htmfile)
+print("<head>", file=htmfile)
+print("<title>", file=htmfile)
+
+sect=0
+for i in range(55):
+	d=0
+	k1=re.match(r'\\title{',lw[i])
+	if(k1):
+		d=1
+		print(lw[i][k1.end():-1], file=htmfile)
+		print("</title></head><body><div id='ttle'><h1>", file=htmfile)
+		print(lw[i][k1.end():-1], file=htmfile)
+		print("</h1>", file=htmfile)
+	k1=re.match(r'\\author{',lw[i])
+	if(k1):
+		d=1
+		print("</div><div id='auth'><h1>", file=htmfile)
+		print(lw[i][k1.end():-1], file=htmfile)
+		print("</h1>", file=htmfile)
+	k1=re.match(r'\\section{', lw[i])
+	if(k1):
+		d=1
+		print("</div><div id='sec'><h1>", file=htmfile)
+		print(lw[i][k1.end():-1], file=htmfile)
+		print("</h1>", file=htmfile)
+	k1=re.match(r'\\subsection{', lw[i])
+	if(k1):
+		d=1
+		print("</div><div id='subsec'><h1>", file=htmfile)
+		print(lw[i][k1.end():-1], file=htmfile)
+		print("</h1>", file=htmfile)
+		
+	k1=re.match(r'\\includegraphics\[.*\]{', lw[i]);
+	if(k1):
+		d=1
+		
+	k1=re.match(r'[a-zA-Z\<]',lw[i])
+	if(k1):
+		print(lw[i], file=htmfile);
+
+
+
+print("</div><div><h1>Plot 1</h1><img src='../plots/g07_lab09_plot01.png'></div>", file=htmfile)
+
+print("<div><h1>Plot 2</h1><img src='../plots/g07_lab09_plot02.png'></div>", file=htmfile)
+
+print("<div><h1>Plot 3</h1><img src='../plots/g07_lab09_plot03.png'></div>", file=htmfile)
+
+print("<div><h1>Plot 4</h1><img src='../plots/g07_lab09_plot04.png'></div>", file=htmfile)
+
+print("<div><h1>Plot 5</h1><img src='../plots/g07_lab09_plot05.png'></div>", file=htmfile)
+print("</body></html>", file=htmfile)
+#close </div>, </body>, </html>
+#Add images regex
+
+		
+
+
+		
+
+		
